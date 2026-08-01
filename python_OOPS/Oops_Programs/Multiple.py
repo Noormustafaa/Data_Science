b8e56ff94048cb1eedf8805339e591dd8052c477
Noormustafaa <musawirpresents@gmail.com>
2026-08-01 22:28:00 +0500

    just variable changes

--- python_OOPS/Oops_Programs/Multiple.py
+++ python_OOPS/Oops_Programs/Multiple.py
@@ -1,16 +1,16 @@
-class A:
+class K:
     varA ="Weclome To Class A"
 
 class B: 
     varB = "Welcome to class B"
 
 
-class C(A, B):
+class C(K, B):
     varc="Welcome to class C"
   
   
 c1 = C()
   
 print(c1.varc)
-print(c1.varA)
+print(c1.varK)
 print(c1.varB)
