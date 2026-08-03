b4eeb2b04c9e5dbdb9e8c01e1c30a2eebab69ad6
Noor Mustafa Rajpar <150359415+Noormustafaa@users.noreply.github.com>
2026-08-03 22:43:18 +0500

    Refactor static method in Student class
    Updated static method definition and added explanation.

--- python_OOPS/Oops_Programs/staticmethod.py
+++ python_OOPS/Oops_Programs/staticmethod.py
@@ -1,8 +1,9 @@
+#methods that don't use the self parameter are called static methods. Static methods are defined using the @staticmethod decorator and can be called on the class itself, rather than on an instance of the class. They do not have access to the instance (self) or class (cls) variables and are typically used for utility functions that don't require any information about the instance or class.
 class Student:
-	       #decoder 
-     @staticmethod
-     def collage():
-             print("ABC College")
+    @staticmethod #decorator # for Removing error we use @staticmethod decorator. It is used to define a static method in a class. A static method is a method that belongs to the class rather than an instance of the class. It does not have access to the instance (self) or class (cls) variables and can be called on the class itself, rather than on an instance of the class.
+    def college():
+        print("this is a static method Bro")
 
 
-Student.collage()
+# Static method ko DIRECT Class name se call kar sakte hain (Bina object banaye)
+Student.college() # we can call the static method directly using the class name without creating an instance of the class.
